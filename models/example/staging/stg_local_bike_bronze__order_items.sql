8cb4aa554fda23c820c08dd888ce326069675d74
PaulInsight <paulsouchard@gmail.com>
2025-02-27 14:29:36 +0100

    fix: remove column item_id useless in order_item

--- models/example/staging/stg_local_bike_bronze__order_items.sql
+++ models/example/staging/stg_local_bike_bronze__order_items.sql
@@ -11,7 +11,6 @@ result as (
     select
         cast(concat(order_id, '_', item_id) as string) AS order_item_id,
         cast(order_id as string) as order_id,
-        cast(item_id as string) as item_id,
         cast(product_id as string) as product_id,
         cast(quantity as numeric) as quantity,
         cast(list_price as numeric) as price,
