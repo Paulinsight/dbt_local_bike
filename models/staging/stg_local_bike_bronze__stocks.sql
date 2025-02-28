e9c2b55b88fb5eb46b3ce3b4a00fc290f5628efa
PaulInsight <paulsouchard@gmail.com>
2025-02-28 09:18:46 +0100

    add mrt_stocks

--- models/staging/stg_local_bike_bronze__stocks.sql
+++ models/staging/stg_local_bike_bronze__stocks.sql
@@ -12,7 +12,7 @@ result as (
         cast(concat(store_id, '_', product_id) as string) AS stock_id,
         cast(store_id as string) as store_id,
         cast(product_id as string) as product_id,
-        cast(quantity as int64) as quantity
+        cast(quantity as int64) as stock
 
     from source
 
