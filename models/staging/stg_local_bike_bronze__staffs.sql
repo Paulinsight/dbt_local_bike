9c838a8892558211cb9adf20ca516a1d67cb3eb6
PaulInsight <paulsouchard@gmail.com>
2025-02-28 08:59:17 +0100

    add staff in stg...staffs

--- models/staging/stg_local_bike_bronze__staffs.sql
+++ models/staging/stg_local_bike_bronze__staffs.sql
@@ -12,8 +12,9 @@ result as (
         cast(staff_id as string) as staff_id,
         cast(store_id as string) as store_id,
         cast(manager_id as string) as manager_id,
-        cast(first_name as string) as first_name,
-        cast(last_name as string) as last_name,
+        cast(concat(first_name, ' ', upper(last_name), ' (', staff_id, ')') as string) as staff,
+        --cast(first_name as string) as first_name,
+        --cast(last_name as string) as last_name,
         cast(email as string) as email,
         cast(phone as string) as phone,
         cast(active as int64) as active
